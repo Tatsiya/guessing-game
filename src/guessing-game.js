94b1a9d2f2eb30fefb9ec0fb8e60b2225cbcbf35
Tatsiya <tanya.pavlovskaya.97@mail.ru>
2017-09-26 23:54:13 +0300

    Guessing-game task is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.array = [];
+    }
 
     setRange(min, max) {
-
+        var numberOfElements = max - min + 1;
+        var value = min;
+        for (var i = 0; i < numberOfElements; i++) {
+            this.array.push(value);
+            value++;
+        }
+        return this.array;
     }
 
     guess() {
-
+        var candidate = this.array[Math.floor(this.array.length / 2)];
+        return candidate;
     }
 
     lower() {
-
+        this.array = this.array.slice(0, Math.floor((this.array.length / 2)+1));
     }
 
     greater() {
-
+        this.array = this.array.slice(Math.floor(this.array.length / 2), this.array.length);
     }
 }
 
